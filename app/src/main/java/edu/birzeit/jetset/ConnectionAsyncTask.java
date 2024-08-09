dd4e1ed9c9d171816fb75b00814a4a7e8e50850e
samasthtc <samasthtc@gmail.com>
2024-08-09 15:22:01 +0300

    Initial Commit

---
+++ app/src/main/java/edu/birzeit/jetset/ConnectionAsyncTask.java
@@ -0,0 +1,36 @@
+package edu.birzeit.jetset;
+
+import android.app.Activity;
+import android.os.AsyncTask;
+
+import java.util.List;
+
+public class ConnectionAsyncTask extends AsyncTask<String, String, String> {
+    Activity activity;
+
+    public ConnectionAsyncTask(Activity activity) {
+        this.activity = activity;
+    }
+
+    @Override
+    protected void onPreExecute() {
+//        ((MainActivity) activity).setButtonText("connecting");
+//        super.onPreExecute();
+//        ((MainActivity) activity).setProgress(true);
+    }
+
+    @Override
+    protected String doInBackground(String... params) {
+        return HttpManager.getData(params[0]);
+    }
+
+    @Override
+    protected void onPostExecute(String s) {
+        super.onPostExecute(s);
+//        ((MainActivity) activity).setProgress(false);
+//        ((MainActivity) activity).setButtonText("connected");
+//        List<Student> students =
+//                StudentJsonParser.getObjectFromJson(s);
+//        ((MainActivity) activity).fillStudents(students);
+    }
+}
